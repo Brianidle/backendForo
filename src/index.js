e34855cc1af1c1e215416a2b452104bc5148e05d
SctBrian <ddbrayan10@hotmail.com>
2021-02-27 19:46:46 -0300

    El backend comienza a usar ApolloServer para poder recibir, entender y responder, usando los schemas y los resolvers, las peticiones del cliente. Se agrega como variable 'global' de graphql a models, asi puedo crear nuevos documentos en las colecciones de la base de datos mongodb

--- src/index.js
+++ src/index.js
@@ -1,15 +1,30 @@
+const typeDefs = require('./schema');
+const resolvers = require('./resolvers');
+const { ApolloServer } = require('apollo-server-express');
+
 require('dotenv').config();
 
 const db = require('./db');
 const express = require('express');
-const port = process.env.PORT || 4000;
 
+const port = process.env.PORT || 4000;
+const DB_HOST = process.env.DB_HOST;
+
+const models = require('./models');
 //---------------------------------------------------------------------
 
 const app = express();
-
-const DB_HOST = process.env.DB_HOST;
 db.connect(DB_HOST);
+
+const apolloServer = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: () => {
+    return { models };
+  }
+});
+
+apolloServer.applyMiddleware({app, path:'/'});
 
 app.listen({ port }, () =>
   console.log(
