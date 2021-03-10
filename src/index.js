a62538c31a54ca2ef02a1e3c218530e2ad96c2e6
SctBrian <ddbrayan10@hotmail.com>
2021-03-10 11:37:56 -0300

    Now, The Server takes the value of the Authorization header of all requests and set it on the graphQL context.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const typeDefs = require('./schema');
 const resolvers = require('./resolvers');
+const jwt = require('jsonwebtoken');
+
 const cors = require('cors');
 const { ApolloServer } = require('apollo-server-express');
 
@@ -22,15 +24,30 @@ db.connect(DB_HOST);
 const apolloServer = new ApolloServer({
   typeDefs,
   resolvers,
-  context: () => {
-    return { models };
+  context: ({ req }) => {
+    
+    const token = req.headers.authorization;
+    
+    const idUser = getUser(token);
+    
+    return { models, idUser };
   }
 });
 
-apolloServer.applyMiddleware({app, path:'/foroApi'});
+apolloServer.applyMiddleware({ app, path: '/foroApi' });
 
 app.listen({ port }, () =>
   console.log(
     `GraphQL Server running at http://localhost:${port}${apolloServer.graphqlPath}`
   )
 );
+
+const getUser = token => {
+  if (token) {
+    try {
+      return jwt.verify(token, process.env.JWT_SECRET);
+    } catch (err) {
+      throw new Error('Session invalid');
+    }
+  }
+};
